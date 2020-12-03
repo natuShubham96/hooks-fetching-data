e5a2b13779342384aa097a341800461535080950
snatu1996 <shubham.natu@cognizant.com>
2020-12-03 16:18:43 +0530

    Using async await for API data fetching

--- src/App.js
+++ src/App.js
@@ -4,9 +4,33 @@ import axios from 'axios';
 export default function App () {
     const [lists,setLists] = useState([])
 
-    useEffect(() => {
-axios.get("https://hn.algolia.com/api/v1/search?query=reacthooks").then(response => setLists(response.data.hits));    
-},[ ])
+
+    //Using .then for promise resolution
+
+//     useEffect(() => {
+// axios.get("https://hn.algolia.com/api/v1/search?query=reacthooks").then(response => setLists(response.data.hits));    
+// },[ ])
+
+//Using async await for promise resolution
+
+//using async as part of useEffect will give us a warning, hence create a separate async function and call it inisde useEffect
+
+// useEffect(async () => {
+//     const result = await axios.get("https://hn.algolia.com/api/v1/search?query=reacthooks")
+
+//     setLists(result.data.hits)
+// })
+
+useEffect(() => {
+    getData()
+},[])
+
+const getData = async () => {
+    const result = await axios.get("https://hn.algolia.com/api/v1/search?query=reacthooks")
+
+    setLists(result.data.hits)
+}
+
     return (
         <div>
             {lists.map(data => <ul>
